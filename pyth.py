1e48af840bf7987500e2b5f3d38b0119b38d4a00
white_Mad_666 <85440691+akhil239@users.noreply.github.com>
2024-12-14 04:31:36 +0000

    pyht.py

--- pyth.py
+++ pyth.py
@@ -1,1 +1,13 @@
+import math
 print("hsdkfh")
+
+class Myc();
+    def __init__(self,name ,date,moth):
+        self.name = name
+        self.date = date 
+        self.moth = moth
+
+    def my_mthod(self):
+        print(self.name)
+intance = Myc('akhil','22','7')
+intance.my_mthod()        
