3f7f33818d1432da2aa856ff3c9e64a1700bf262
white_Mad_666 <85440691+akhil239@users.noreply.github.com>
2025-01-14 06:48:27 +0000

    pyth.py

--- algo.c
+++ algo.c
@@ -2,17 +2,25 @@
 #include<stdlib.h>
 
 
+
 struct Node
 {   int data;
     struct Node *next;
     /* data */
 };
 int add_node(int data);
+void traverse();
 struct Node *ptr = NULL;
 
 int main(){
-    return 0;
     add_node(50);
+    add_node(500);
+    add_node(570);
+    add_node(545);
+    add_node(5370);
+    printf("%d",ptr->data);
+    traverse();
+    return 0;
 }
 
 int add_node(int data){
@@ -20,11 +28,26 @@ int add_node(int data){
         ptr = (struct Node *)malloc(sizeof(struct Node));
         ptr->data = data;
         ptr->next = NULL;
-        printf("new as created sccuessfully...");
+        printf("new node as created sccuessfully...");
+        return 0;
+    }else{
+        struct Node *current = ptr;
+        while(current->next != NULL){
+            current = current->next;
+        }
+        current->next = (struct Node *)malloc(sizeof(struct Node));
+        current->next->data = data;
+        current->next->next = NULL;
     }
 
 }
 void traverse(){
-
+    struct Node *current = ptr;
+    while (current != NULL)
+    {   printf("\n%d",current->data);
+        current = current->next;
+        /* code */
+    }
+    
 
 }
