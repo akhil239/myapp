bb0a187b8fd5a0cbfec9ee9e0c17a230c1fef107
white_Mad_666 <85440691+akhil239@users.noreply.github.com>
2025-01-14 06:12:19 +0000

    pyth.py

--- algo.c
+++ algo.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct Node
+{   int data;
+    struct Node *next;
+    /* data */
+};
+int add_node(int data);
+struct Node *ptr = NULL;
+
 int main(){
     return 0;
+    add_node(50);
 }
 
 int add_node(int data){
+    if(ptr == NULL){
+        ptr = (struct Node *)malloc(sizeof(struct Node));
+        ptr->data = data;
+        printf("new as created sccuessfully...");
+    }
 
 }
 void traverse(){
-    
+
 
 }
