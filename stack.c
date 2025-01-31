388aadd14e0e930b0b03b54038ede702884b5d79
white_Mad_666 <85440691+akhil239@users.noreply.github.com>
2025-01-31 13:07:47 +0000

    pyth.py

--- stack.c
+++ stack.c
@@ -11,17 +11,19 @@ void push(int value){
 //this function will remove element from stack
 void pop(){
     
-    top = top-=1;//this  
+    top = top-=1;//this  subtract 1 from top 
+    
 }
 
 int main(){
     n = 5;//n = 5 length of this stack is 5
     stack = (int *)malloc(n * sizeof(int)); //this points to dynamic array this array can hold upot 5 elements
-    top = -1;
-    push(5);
-    push(89);
-    pop();
+    top = -1;//if we don't use negitve number we start from inserting element's from 1 so  that's way we negitive number 
+    push(5);//this will  add number; 5 to the stack
+    push(89);// this will add number ; 8 to the stack
+    pop();// this will pop element from the stack by subtrating the top 
     // printf("%d",top);
+    //this will prints element's persent in the stack
     for(int i = 0;i <= top;i++){
         printf("%d",stack[i]);
     }
