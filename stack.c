519f77f7714c9a82835e6ecebd61ba60a64b0c86
white_Mad_666 <85440691+akhil239@users.noreply.github.com>
2025-01-31 13:22:09 +0000

    pyth.py

--- stack.c
+++ stack.c
@@ -4,6 +4,7 @@ int n; // number of eleement
 int *stack; //globel stack pointer 
 int top;//this show the top element in the stack 
 //this function add element to the stack
+int first;
 void push(int value){
     top = top+= 1;//this will increment top by 1 so that we can use this to track stack element's
     stack[top] = value;//
@@ -19,18 +20,25 @@ void pop(){
     by using this code so that we overwrite that element after dicrseing the stack 
     we need to call push again this overwrite that element with new one*/
 }
+void queuepush(int value){
+    stack[top] = value;
+    top = top-=1;
+}
+void queuepop(){
+    first -=1;
+}
 
 int main(){
     n = 5;//n = 5 length of this stack is 5
     stack = (int *)malloc(n * sizeof(int)); //this points to dynamic array this array can hold upot 5 elements
     top = -1;//if we don't use negitve number we start from inserting element's from 1 so  that's way we negitive number 
-    push(5);//this will  add number; 5 to the stack
-    push(89);// this will add number ; 8 to the stack
+    first = -1;
+    queuepush(5);//this will  add number; 5 to the stack
+    queuepush(89);// this will add number ; 8 to the stack
     pop();// this will pop element from the stack by subtrating the top 
-    push(897);
     // printf("%d",top);
     //this will prints element's persent in the stack
-    for(int i = 0;i <= top;i++){
+    for(int i = n;i >= top;i--){
         printf("%d",stack[i]);
     }
     return 0;
