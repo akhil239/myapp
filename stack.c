a74ae5e7d61feb5311b1c5109c605dc96b97e328
white_Mad_666 <85440691+akhil239@users.noreply.github.com>
2025-01-31 12:47:53 +0000

    pyth.py

--- stack.c
+++ stack.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
-n = 5; // length
-int *stack = (int *)malloc()
+int n; // length
+int *stack;
+int top;
 void push(int value){
-
-
+    top = top+= 1;
+    stack[top] = value;
 }
 
 int main(){
+    n = 5;
+    stack = (int *)malloc(n * sizeof(int));
+    top = -1;
+    push(5);
+    push(89);
+    printf("%d",stack[0]);
+    printf("%d",stack[1]);
     return 0;
 }
