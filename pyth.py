2bb8ef684e68523bc1792ab56a2ab5cb55e9a253
white_Mad_666 <85440691+akhil239@users.noreply.github.com>
2025-06-24 13:00:01 +0000

    pyth.py

--- pyth.py
+++ pyth.py
@@ -18,11 +18,24 @@ class Linked_list():
                 current_node.next = Node(data)
     def _print_list(self):
         current_node = self.root
+        count = 0
         while current_node != None:
+            count += 1
             print(current_node.data)
             current_node = current_node.next
+        return count 
+    def add_node_at_front(self,data):
+        current_node = self.root
+        self.root = Node(data)
+        self.root.next = current_node 
+        del current_node
+    def add_node_at_middle():
+        
+    def add_node_at_last():
+        pass
 
 obj = Linked_list()
 obj.add_linked_list(5)
 obj.add_linked_list(665)
+obj.add_node_at_front(55)
 obj._print_list()
