d86e35d9f63fdcf7f563f5db89d28c07d1d2c709
white_Mad_666 <85440691+akhil239@users.noreply.github.com>
2025-06-24 12:53:09 +0000

    pyth.py

--- pyth.py
+++ pyth.py
@@ -1,16 +1,14 @@
-import math
-print("hsdkfh")
-
 class Node():
-    def __init__(self,data,next):
+    def __init__(self,data):
         self.data = data
         self.next = None
 
 class Linked_list():
-    self.root = None
+    def __init__(self):
+        self.root = None
 
     def add_linked_list(self,data):
-        if self.root = None:
+        if self.root == None:
             self.root = Node(data)
         else:
             current_node = self.root
@@ -18,6 +16,13 @@ class Linked_list():
                 current_node = current_node.next
             else:
                 current_node.next = Node(data)
+    def _print_list(self):
+        current_node = self.root
+        while current_node != None:
+            print(current_node.data)
+            current_node = current_node.next
 
-
-obj = 
+obj = Linked_list()
+obj.add_linked_list(5)
+obj.add_linked_list(665)
+obj._print_list()
